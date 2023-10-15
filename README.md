05873d7d883e4fd74c526089bf37848639ad4ae7
Donald Motswiri <rmotswiri023@student.wethinkcode.co.za>
2023-10-15 23:14:23 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # scrimba_take_home
 scrimba_take_home
+https://different-marmoset-f7b.notion.site/Take-Home-Assignment-For-Potential-Scrimba-Teachers-40eb4404e92d48f89abf7459d2e1843d
+
+https://freefrontend.com/css-lists/
 
 STICKY DEFINITION LIST
 httpsSTICKY DEFINITION LIST://codepen.io/chriscoyier/pen/gOxgYxO
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2>Grandma's delicious pancakes</h2>
+      <ul>
+        <li>🥛 milk</li>
+        <li>🥚 eggs</li>
+        <li>🥣 flour</li>
+        <li>🐟 fish</li>
+        <li>🧈 butter</li>
+      </ul>
+      <p>Grandma's Delicious Recipies ™️</p>
+    </div>
+  );
+}
+
+export default App;
